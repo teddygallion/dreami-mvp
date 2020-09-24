656f330d8d63a78785e22f1dbc8dc52d87132674
Teddy Gallion <teddygallion@gmail.com>
2020-09-23 20:26:40 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,62 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import React, {useState, useEffect} from 'react';
+
 function App() {
+  const [dreams, setDreams] = useState(false);
+  useEffect(() => {
+    getDreams();
+  }, []);
+  function getDreams() {
+    fetch('http://localhost:3001')
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        setDreams(data);
+      });
+  }
+  function createDream() {
+    let name = prompt('Dreams');
+    fetch('http://localhost:3001/dreams', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({title, content}),
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        alert(data);
+        getDreams();
+      });
+  }
+  function deleteDream() {
+    let id = prompt('Enter task id');
+    fetch(`http://localhost:3001/tasks/${id}`, {
+      method: 'DELETE',
+    })
+      .then(response => {
+        return response.text();
+      })
+      .then(data => {
+        alert(data);
+        getDreams();
+      });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {tasks ? tasks : 'There are no tasks available'}
+      <br />
+      <button onClick={createDream}>Add Dream</button>
+      <br />
+      <button onClick={deleteDream}>Delete Dream</button>
     </div>
   );
 }
-
 export default App;
+
+
